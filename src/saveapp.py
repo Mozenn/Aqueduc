5f96dee94a6f3cc658107c3d163cb374ce466c61
Cassany Gauthier <gauthier.cassany@gmail.com>
2020-04-24 10:33:50 +0200

    Options done except last modification date

--- src/saveapp.py
+++ src/saveapp.py
@@ -26,45 +26,64 @@ def main():
 
         for source in plan["sources"]:
 
-            if not(os.path.exists(source["path"])) or not(os.path.isfile(source["path"])):
+            if not(os.path.exists(source["path"])) and not(os.path.isfile(source["path"])):
                 print("Error in Plan File. Invalid source")
                 sys.exit(1) 
 
-            move(source["path"],source["options"])
+            move(source["path"],source["options"],target_path,1)
 
 
 
 
-if not(source["overwrite"]):
-    source_name = source["path"].split(os.path.sep)[-1]
+#    new_path = [ el for i,el in enumerate(path.split(os.path.sep)) if i > 3]
+#    print(os.path.sep.join(new_path))
 
-    if os.path.exists(target_path + os.path.sep + source_name):
-        continue 
-
-def move(path,options) : 
+def move(path,options,target_path,depth) : 
 
     if os.path.isfile(path) : 
 
         extension = path.split(".")[-1]
 
         if extension in options["forbidden_extensions"] : 
+            print("forbidden extension")
             return 
-        # TODO check size => search how to 
-        # TODO check extension => use split 
-        
-        # TODO check overwrite => tweak above code 
 
-        shutil.copy(path,target_path)
-        
-    elif os.path.isdir(source["path"]) :
+        file_size = os.path.getsize(path)
+        if options["size_limit"] != -1 and file_size >= options["size_limit"]:
+            print("too large")
+            return 
 
-        entries = [path + os.path.sep + x) for x in os.listdir(path)]
+        # TODO check last modification time =>os.path.getmtime(path)
+
+        cut_path_list = [ el for i,el in enumerate(path.split(os.path.sep)) if i >= depth ]
+        
+        if not options["overwrite"] : 
+        
+            cut_path = os.path.sep.join(cut_path_list)
+            
+            if os.path.exists(target_path + os.path.sep + cut_path):
+                print("not overwrite")
+                return  
+
+        cut_path_list_copy = cut_path_list.copy()
+        cut_path_list_copy.pop(-1)
+        dir_to_create = target_path + os.path.sep + os.path.sep.join(cut_path_list_copy)
+        print("CREATED ",dir_to_create)
+        os.makedirs(dir_to_create)
+
+        final_target_path = target_path + os.path.sep + os.path.sep.join(cut_path_list)
+        print(final_target_path)
+        shutil.copy(path,final_target_path) # TODO Fix error 
+        
+    elif os.path.isdir(path) :
+
+        entries = [path + os.path.sep + x for x in os.listdir(path)]
 
         for entry in entries : 
-            move(entry,options)
+            move(entry,options,target_path,depth+1)
 
 def check_parameter() :
-        if len(sys.argv) <= 1:
+    if len(sys.argv) <= 1:
         print("Wrong input. Input should be in form : SaveProgram PlanPath")
         sys.exit(1) 
 
--- src/test.py
+++ src/test.py
@@ -20,6 +20,9 @@ def main():
 if __name__== "__main__":
    #main()
    path = "C:\\Users\\Gauthier\\Downloads\\Music"
+   
+   new_path = [ el for i,el in enumerate(path.split(os.path.sep)) if i > 2]
+   print(os.path.sep.join(new_path))
    child_paths = os.listdir(path)
-   [print(path + os.path.sep + x) for x in child_paths]
-   [print(x.split(".")[-1]) for x in child_paths]
+#    [print(path + os.path.sep + x) for x in child_paths]
+#    [print(x.split(".")[-1]) for x in child_paths]
